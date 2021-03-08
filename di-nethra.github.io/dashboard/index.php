c4e808196b033c25e7fb5b50819f311169eab5ee
dinethra <61901807+di-nethra@users.noreply.github.com>
2021-03-08 14:28:35 +0530

    initial commit

---
+++ di-nethra.github.io/dashboard/index.php
@@ -0,0 +1,27 @@
+<?php include_once "inc/header.php" ?>
+<?php include_once "inc/sidebar.php" ?>
+            <div class="row">
+                <div class="col-4">
+                    <div class="content-box">
+                        <div class="section-heading">
+                            <h5>sample heading <span>test</span></h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="content-box">
+                        <div class="section-heading">
+                            <h5>sample heading</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="content-box">
+                        <div class="section-heading">
+                            <h5>sample heading</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+<?php include_once "inc/footer.php" ?>
